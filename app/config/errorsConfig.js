b9506d0a424b60dc47bfa8bf01fde1bc31df4092
Arman Abbasian <abasian.arman@gmail.com>
2023-07-17 10:15:05 +0330

    make the error handling section

--- app/config/errorsConfig.js
+++ app/config/errorsConfig.js
@@ -1,8 +1,20 @@
-const { NotFoundError, ErrorHandler } = require("../http/middlewares/errorHandling");
+const createError=require("http-errors")
 
 function errorHandler(app){
-app.use(NotFoundError);
-app.use(ErrorHandler)
+    app.use((req, res, next) => {
+        next(createError.NotFound("Not Found"));
+      });
+      app.use((error, req, res, next) => {
+        const serverError = createError.InternalServerError();
+        const statusCode = error.status || serverError.status;
+        const message = error.message || serverError.message;
+        return res.status(statusCode).json({
+          statusCode,
+          errors: {
+            message,
+          },
+        });
+      });
 }
 module.exports={
     errorHandler
