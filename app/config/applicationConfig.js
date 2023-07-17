5c33e1cc423f794316f32441dbaf581266b1a8c9
Arman Abbasian <abasian.arman@gmail.com>
2023-07-17 09:44:00 +0330

    some refactor on configApplication and connectToMongoDB section

--- app/config/applicationConfig.js
+++ app/config/applicationConfig.js
@@ -1,5 +1,9 @@
+const morgan = require("morgan");
+
 function configApplication(app,express){
     const path=require("path")
+    //morgan package to log all client requests
+    app.use(morgan("dev"));
     //settings for the client send files
     app.use(express.json());
     app.use(express.urlencoded({extended:true}));
--- app/config/databaseConfig.js
+++ app/config/databaseConfig.js
@@ -6,6 +6,19 @@ mongoose.connect(DB_URL)
 .then(()=>console.log("mongoose connected to mongodb successfully"))
 .catch((err)=>console.log(err))
 };
+//connected event for mongoose
+mongoose.connection.on("connected", () => {
+    console.log("mongoose connected to mongodb");
+  });
+  //dieconnected event for mongoose
+  mongoose.connection.on("disconnected", () => {
+    console.log("mongoose  disconnected");
+  });
+  process.on("SIGINT", async () => {
+    await mongoose.connection.close();
+    console.log("disconnected");
+    process.exit(0);
+  });
 module.exports={
     configDataBase
 }
